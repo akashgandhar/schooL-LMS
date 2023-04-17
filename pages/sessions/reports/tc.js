52add8bc2ba7e790aa69608a2c450f85e25caf4b
school-management-868 <smanagement868@gmail.com>
2023-04-17 17:41:25 +0530

    tc

--- pages/sessions/reports/tc.js
+++ pages/sessions/reports/tc.js
@@ -9,36 +9,75 @@ export default function Tc() {
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
   })
+
+  const fields = [
+    'Name of the Pupil',
+    "Mother's Name",
+    "Father's/Guardian's Name",
+    'Date of Birth according to the Admission Register (In figure)',
+    'Nationality',
+    'Whether the pupil belongs to SC/ST/OBC category',
+    'Date of first admission in the school with class',
+    'Class in which the pupil last studies (in words)',
+    'Whether failed, if so once/twice in same class?',
+    'School/Board Examination last taken with result',
+    'Subjects offered',
+    'Whether qualified for promotion to the next higher class',
+    'Month upto which the pupil has paid school dues?',
+    'Any fee concession availed of? if so the Roture of such concession.',
+    'Total no. of working days',
+    'Total no. of working days present',
+    'Whether the pupil is NCC Cadet/Boy Scunt/Girl Guide (give details)',
+    'Games played or extra curricular activities in which the pupil usually took part (mention bchivement level)',
+    'General conduct',
+    'Application of certificate',
+    'Actual issue of Certificate',
+    'Reason for leaving the school',
+    'Any other remarks',
+    "Date on which pupil's name was struck off rolls of the school",
+  ]
   return (
-    <center className="w-full py-7">
-      <div className="w-full bg-no-repeat bg-center">
-        <div ref={componentRef} id="print" className="pt-20 pb-5">
-          <table className=" align-middle h-full mx-auto w-11/12 border-2 border-black text-lg">
+    <center className="w-full py-7 text-[12pt]">
+      <div className="w-[250mm] max-h-[280mm] bg-no-repeat bg-center">
+        <div ref={componentRef} id="print" className="pt-5 pb-5 ">
+          <table className=" align-middle max-h-[300mm] mx-auto w-11/12 border-2 border-black text-[12pt]">
             <tbody>
               <tr>
                 <td className="h-auto col-span-9 flex justify-between">
                   <img
-                    src="https://firebasestorage.googleapis.com/v0/b/assign-eefa5.appspot.com/o/Akash%20Gandhar%2FLOGOa.png?alt=media&token=0d48043a-f970-4495-b131-5f788bb00733"
-                    className="float-left ml-8 h-28 mt-2"
+                    src="/logoa.png"
+                    className="float-left ml-8 h-28 mt-2 mix-blend-color-burn"
                   />
                   <div className="ml-5  float-right ">
-                    <center className="float-left">
-                      <span className="text-red-600 font-bold text-6xl">
+                    <center
+                      className="float-left"
+                      style={{ lineHeight: '0.5' }}
+                    >
+                      <span
+                        className="text-red-600 font-extrabold text-3xl"
+                        style={{ lineHeight: '1.2' }}
+                      >
                         M J PUBLIC SCHOOL
                       </span>
-                      <span>
+                      <br />
+                      <span
+                        className="font-bold text-xs"
+                        style={{ lineHeight: '1.2' }}
+                      >
+                        Affiliated to Central Board of Secondary Education
+                        (C.B.S.E.)
                         <br />
-                        <span className="font-bold text-base">
-                          Affiliated to Central Board of Secondary Education
-                          (C.B.S.E.) New Delhi
-                          <br />" Website: www.mjpssadabad.in mail id:
-                          mjpssadabad.cbse@gmail.com "
-                          <br />
-                          <span className="text-2xl">
-                            RAYA ROAD SADABAD HATHRAS
-                          </span>
+                        mail id: mjpssadabad.cbse@gmail.com"
+                        <br />
+                        <span className="text-xs">
+                          RAYA ROAD SADABAD HATHRAS
                         </span>
                       </span>
+                      <div className="font-bold text-2xl italic mt-2 ml-2 w-96 border-2 border-black bg-white rounded-2xl">
+                        <div className="pt-0.5 text-lg">
+                          <span className="ml-2">TRANSFER CERTIFICATE</span>
+                        </div>
+                      </div>
                     </center>
                   </div>
                   <img
@@ -47,17 +86,17 @@ export default function Tc() {
                   />
                 </td>
               </tr>
-              <tr>
-                <td align="center" height="62" className="col-span-9">
+              {/* <tr>
+                <td align="center" height="20" className="col-span-9">
                   <div className="font-bold text-2xl italic ml-2 w-96 border-2 border-black bg-white rounded-2xl">
-                    <div className="pt-0.5 text-2xl">
+                    <div className="pt-0.5 text-lg">
                       <span className="ml-2">TRANSFER CERTIFICATE</span>
                     </div>
                   </div>
                 </td>
-              </tr>
+              </tr> */}
               <tr>
-                <td height="30"> </td>
+                <td height="5"> </td>
               </tr>
               <tr className="font-bold flex justify-between px-5">
                 <td>School No. : 60978</td>
@@ -70,224 +109,32 @@ export default function Tc() {
               </tr>
               <tr className=" flex justify-between px-5">
                 <td height="39" colSpan="7" valign="top">
-                  {/* Registration No. of the candidate (in case Class-IX to XII) */}
+                  Registration No. of the candidate (in case Class-IX to XII) :
                 </td>
                 <td colSpan="2" valign="top"></td>
               </tr>
 
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  1.
-                  <span> Name of Student </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>{s.name} </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5 "
-              >
-                <td height="34" colSpan="6" className="flex-1 ">
-                  2.
-                  <span> Mother's Name </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right ">
-                  <b>{s.Mother_Name} </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  3.
-                  <span> Father's Name </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>{s.Father_Name} </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  4.
-                  <span> Nationality </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>Indian </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  5.
-                  <span> Category </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>{s.Category} </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  6.
-                  <span> Date of Birth </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>{s.Date_Of_Birth} </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  7.
-                  <span> Date of Admission </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>{s.Admission_Date} </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  8.
-                  <span> pupil is failed or pass </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>Pass </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  9.
-                  <span> Last Class </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>{s.Class} </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  10.
-                  <span> School/Board </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>MJ Public School / CBSE </b>
-                </td>
-              </tr>
+              {fields.map((e, index) => {
+                return (
+                  <tr key={index} className=" flex px-5 ">
+                    <td colSpan="6" className="flex-1 ">
+                      {index + 1}.<span> {e} </span>
+                    </td>
+                    <td colSpan={3} className="flex-1 h-auto ">
+                      <span className="font-bold W-[10%]"> : </span>
 
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  11.
-                  <span> Promoted or Not </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>Pramoted </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  12.
-                  <span> Total Attendance (in %) </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b> 75% </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  13.
-                  <span> Date of application </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>DATE </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  14.
-                  <span> Date of issue of certificate </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>DATE </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  15.
-                  <span> Reason for leaving </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>GIVEN reason </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1">
-                  16.
-                  <span> General Conduct </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>Good </b>
-                </td>
-              </tr>
-              <tr
-                style={{ marginLeft: '20%', marginRight: '20%' }}
-                className="flex px-5"
-              >
-                <td height="34" colSpan="6" className="flex-1 ">
-                  17.
-                  <span> Any other remark </span>
-                </td>
-                <td height={16} colSpan={3} className="flex-1 text-right">
-                  <b>N/A</b>
-                </td>
-              </tr>
+                      <input
+                        type="text"
+                        className="uppercase w-[90%] text-[10pt] font-bold border-b-2 border-black border-dashed"
+                        placeholder="max 30 words"
+                      />
+                    </td>
+                  </tr>
+                )
+              })}
 
               <tr>
-                <td height="100" colspan="9">
-                  {' '}
-                </td>
-              </tr>
-              <tr>
-                <td height="100" colspan="9">
+                <td height="20" colspan="9">
                   {' '}
                 </td>
               </tr>
@@ -306,9 +153,13 @@ export default function Tc() {
             </tbody>
           </table>
         </div>
+        <button
+          class="text-white  bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={handlePrint}
+        >
+          Print
+        </button>
       </div>
-
-      <button class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={handlePrint}>Print</button>
     </center>
   )
 }
