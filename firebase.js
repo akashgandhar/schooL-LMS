b5b8f8066fb6ac5942cfc634fe693f92a28f82f9
akashgandhar <akashgandhar007@gmail.com>
2024-05-11 20:23:56 +0530

    envs

--- firebase.js
+++ firebase.js
@@ -14,24 +14,16 @@ import { getStorage } from "firebase/storage";
 // };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCg00ntkfOqWslxQoRz9BwDhfeBS1erpwE",
-  authDomain: "mjpsmadnai.firebaseapp.com",
-  projectId: "mjpsmadnai",
-  storageBucket: "mjpsmadnai.appspot.com",
-  messagingSenderId: "966036400672",
-  appId: "1:966036400672:web:e8ba5790610e7d6b0b2a45",
-  measurementId: "G-TP9F2YWMR3"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
-// old config
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCF1LFRXq0KmbNrjni6fBBhPH0-7_85FNY",
-//   authDomain: "assign-eefa5.firebaseapp.com",
-//   projectId: "assign-eefa5",
-//   storageBucket: "assign-eefa5.appspot.com",
-//   messagingSenderId: "136399647982",
-//   appId: "1:136399647982:web:1b2f390d09dc6ff0ddefff"
-// };
+
 
 function createFirebaseApp() {
   try {
