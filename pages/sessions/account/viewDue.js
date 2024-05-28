74e31246b6d7a3e27e181243f0eebd0b9e48055d
akashgandhar <akashgandhar007@gmail.com>
2024-05-28 17:11:05 +0530

    table issue reso0lved

--- pages/sessions/account/viewDue.js
+++ pages/sessions/account/viewDue.js
@@ -539,7 +539,7 @@ export default function ViewDue() {
                                     {Number(e.ExamDue) > 0
                                       ? Number(e.ExamDue)
                                       : 0}
-                                  </td></>}
+                                  </td>
                                 <td class="p-2 md:border md:border-grey-500 text-center block md:table-cell">
                                   <span class="inline-block w-1/3 md:hidden font-bold">
                                     ward
@@ -547,7 +547,7 @@ export default function ViewDue() {
                                   {Number(e.WardDue) > 0
                                     ? Number(e.WardDue)
                                     : 0}
-                                </td>
+                                </td></>}
                                 <td class="p-2 md:border md:border-grey-500 text-center block md:table-cell">
                                   <span class="inline-block w-1/3 md:hidden font-bold">
                                     monthly
